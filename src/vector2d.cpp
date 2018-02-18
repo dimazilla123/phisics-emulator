2afedd9f46b136880ee07b019ac875b1cb6b755b
Dima <evtushenko.2002@gmail.com>
2018-02-18 16:50:56 +0300

    refactoring absolutely finished

--- src/vector2d.cpp
+++ src/vector2d.cpp
@@ -1,77 +1,77 @@
 #include "vector2d.hpp"
 #include <math.h>
 
-vector2d :: vector2d()
+vector2d::vector2d ()
 {
     this->x = 0, this->y = 0;
 }
 
-vector2d :: vector2d(double xN, double yN)
+vector2d :: vector2d (double xN, double yN)
 {
     this->x = xN, this->y = yN;
 }
 
-double vector2d :: getX()
+double vector2d::getX ()
 {
     return this->x;
 }
-double vector2d :: getY()
+double vector2d::getY ()
 {
     return this->y;
 }
 
-void vector2d :: setX(double newX)
+void vector2d::setX (double newX)
 {
     this->x = newX;
 }
-void vector2d :: setY(double newY)
+void vector2d::setY (double newY)
 {
     this->y = newY;
 }
 
-double vector2d :: distance()
+double vector2d::distance ()
 {
-    return std::sqrt((this->x) * (this->x) + (this->y) * (this->y));
+    return std::sqrt ((this->x) * (this->x) + (this->y) * (this->y));
 }
-vector2d vector2d :: operator +(vector2d other)
+vector2d vector2d::operator +(vector2d other)
 {
-    vector2d tmp(this->x + other.getX(), this->y + other.getY());
+    vector2d tmp (this->x + other.getX (), this->y + other.getY ());
     return tmp;
 }
-vector2d vector2d :: operator -(vector2d other)
+vector2d vector2d::operator -(vector2d other)
 {
-    vector2d tmp(this->x - other.getX(), this->y - other.getY());
+    vector2d tmp (this->x - other.getX (), this->y - other.getY ());
     return tmp;
 }
-vector2d vector2d :: operator *(double num)
+vector2d vector2d::operator *(double num)
 {
-    vector2d tmp(this->x * num, this->y * num);
+    vector2d tmp (this->x * num, this->y * num);
     return tmp;
 }
-vector2d vector2d :: operator /(double num)
+vector2d vector2d::operator /(double num)
 {
-    vector2d tmp(this->x / num, this->y / num);
+    vector2d tmp (this->x / num, this->y / num);
     return tmp;
 }
 
-vector2d vector2d :: operator +=(vector2d other)
+vector2d vector2d::operator +=(vector2d other)
 {
     *this = (*this + other);
 }
-vector2d vector2d :: operator -=(vector2d other)
+vector2d vector2d::operator -=(vector2d other)
 {
      *this = (*this - other);
 }
-vector2d vector2d :: operator *=(double num)
+vector2d vector2d::operator *=(double num)
 {
     *this = (*this * num);
 }
-vector2d vector2d :: operator /=(double num)
+vector2d vector2d::operator /=(double num)
 {
     *this = (*this / num);
 }
 
-vector2d vector2d :: direction()
+vector2d vector2d::direction ()
 {
-    return *this / this->distance();
+    return *this / this->distance ();
 }
