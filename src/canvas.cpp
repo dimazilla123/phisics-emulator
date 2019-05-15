0d2930453f34751cc02a563233c8959ac670658f
Dmitry <evtushenko.2002@mail.ru>
2019-05-15 19:06:39 +0300

    mouse scrolling and moving added

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -7,20 +7,92 @@ void Canvas::paintEvent (QPaintEvent *event)
     paint.setPen (QPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap));
     for (auto point : body_positions)
     {
-        paint.drawEllipse (point, 10, 10);
+        double x = point.x();
+        double y = point.y();
+        if (x1 <= x && x <= x2 && y1 <= y && y <= y2) {
+            x -= x1;
+            y -= y1;
+            x = w * x / (x2 - x1);
+            y = h * y / (y2 - y1);
+            paint.drawEllipse ({x, y}, 10, 10);
+        }
     }
+}
+
+void Canvas::resizeEvent(QResizeEvent *event)
+{
+    w = event->size().width();
+    h = event->size().height();
+    if (first_resize) {
+        x2 = w;
+        y2 = h;
+        first_resize = false;
+    }
+}
+
+void Canvas::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::MiddleButton) {
+        ispanning = true;
+        anc_x = event->x();
+        anc_y = event->y();
+    }
+}
+
+void Canvas::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::MiddleButton)
+        ispanning = false;
+}
+
+void Canvas::mouseMoveEvent(QMouseEvent *event)
+{
+    if (ispanning) {
+        int x = event->x();
+        int y = event->y();
+        x1 -= (x - anc_x) * (x2 - x1) / w;
+        x2 -= (x - anc_x) * (x2 - x1) / w;
+        y1 -= (y - anc_y) * (y2 - y1) / h;
+        y2 -= (y - anc_y) * (y2 - y1) / h;
+        anc_x = x;
+        anc_y = y;
+    }
+}
+
+void Canvas::wheelEvent(QWheelEvent *event)
+{
+    double cx = (x1 + x2) / 2;
+    double cy = (y1 + y2) / 2;
+    double dx = x2 - cx;
+    double dy = y2 - cy;
+    if (event->delta() < 0) {
+        dx *= 2;
+        dy *= 2;
+    } else {
+        dx *= 0.5;
+        dy *= 0.5;
+    }
+    x1 = cx - dx;
+    x2 = cx + dx;
+    y1 = cy - dy;
+    y2 = cy + dy;
 }
 
 Canvas::Canvas (QWidget *parent) : QWidget (parent)
 {
     this->setStyleSheet ("background-color:white;");
+    x1 = y1 = 0;
+    x2 = this->frameSize().width();
+    y2 = this->frameSize().height();
 }
 Canvas::Canvas () : QWidget ()
 {
     this->setStyleSheet ("background-color:white;");
 }
+
 Canvas::~Canvas ()
 {
+
 }
 
 void Canvas::redraw ()
@@ -28,11 +100,11 @@ void Canvas::redraw ()
     update ();
 }
 
-void Canvas::draw_points (std::vector<vector2d> *points)
+void Canvas::draw_points (const std::vector<vector2d> &points)
 {
     this->body_positions.clear ();
 
-    for (auto point : *points)
+    for (auto point : points)
     {
         double x = point.getX (), y = point.getY ();
         this->body_positions.push_back (QPointF (x, y));
--- src/canvas.hpp
+++ src/canvas.hpp
@@ -4,6 +4,8 @@
 #include "vector2d.hpp"
 #include <vector>
 #include <QVector>
+#include <QResizeEvent>
+#include <QWheelEvent>
 #include <iostream>
 
 /*
@@ -15,11 +17,24 @@ class Canvas : public QWidget
     Q_OBJECT
 private:
     QVector<QPointF> body_positions;
-protected:
-    void paintEvent (QPaintEvent *event);
+    void paintEvent(QPaintEvent *event);
+    void resizeEvent(QResizeEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+
+    int w, h;
+    double x1, y1, x2, y2;
+    bool first_resize = true;
+
+    // for scrolling
+    bool ispanning = false;
+    int anc_x, anc_y;
+    double zoom = 1;
 public:
     QPainter* painter;
-    void draw_points (std::vector<vector2d> *points);
+    void draw_points(const std::vector<vector2d> &points);
     Canvas (QWidget *parent);
     Canvas ();
     ~Canvas();
